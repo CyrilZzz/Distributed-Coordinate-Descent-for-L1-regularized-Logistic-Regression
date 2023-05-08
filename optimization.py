1ad776a5cffc196eef110180ef9b2b8b976f0c71
mmathieu <mathis.mathieu@ensae.fr>
2023-05-08 14:05:30 +0000

    WIP line search

--- optimization.py
+++ optimization.py
@@ -14,22 +14,36 @@ def coordinate_descent(partition,x,w,z,beta,lmbd):
         delta_beta[row[-1]] = t(np.sum(w * x_j * q), lmbd) / np.dot(w, np.power(x_j, 2)) - beta[row[-1]]
     yield delta_beta
 
+def softplus(x):
+    return np.log(1+np.exp(x))
 
-def line_search(delta, f, beta, delta_beta, delta_L_beta,
-                gamma, H_tilde, lmbd, sigma, b):
+def sigmoid2(x):
+    return 1/(1+np.exp(x))
+
+def objective_function(x,y,beta,lmbd):
+    vsoftplus = np.vectorize(softplus(- y * np.matmul(beta,x)))
+    return np.sum(vsoftplus) + lmbd*np.linalg.norm(beta,1)
+
+def line_search(x, y, delta, beta, delta_beta,
+                gamma, lmbd, sigma, b):
     """Creuser plus étape 1. de l'algo 3
-    coder la fonction f (somme de log exponentielle de ...
     calculer de delta_L_beta dans la fonction
     calcul de la hessienne H_tilde : cf equation (3)"""
-    alpha_init = minimize(f, np.array([0.5]), bounds=((delta, 1),))
-    premier_terme = np.dot(delta_L_beta, delta_beta)
-    deuxieme_terme = gamma * np.dot(delta_beta, np.matmul(H_tilde, delta_beta))
-    troisieme_terme = lmbd * (np.linalg.norm(beta + delta_beta, 1) - np.linalg.norm(beta, 1))
-    D = premier_terme + deuxieme_terme + troisieme_terme
-    alpha = alpha_init
-    for j in range(10**5):
-        alpha_candidat = alpha_init * b**j
-        if f(beta + alpha_candidat * delta_beta) <= f(beta) + alpha_candidat * sigma * D:
-            if alpha_candidat > alpha:
-                alpha = alpha_candidat
-    return alpha
+    # Step 2
+    def eval_objective_function(alpha):
+        return objective_function(x, y, beta + alpha*delta_beta, lmbd)
+    alpha_init = minimize(eval_objective_function, np.array([0.5]), bounds=((delta, 1),))
+
+    # Step 3 : Armijo Rule
+    # gradient = np.sum(np.vectorize(sigmoid2(y*np.matmul(beta,x))))
+    # premier_terme = np.dot(gradient, delta_beta)
+    # deuxieme_terme = gamma * np.dot(delta_beta, np.matmul(H_tilde, delta_beta))
+    # troisieme_terme = lmbd * (np.linalg.norm(beta + delta_beta, 1) - np.linalg.norm(beta, 1))
+    # D = premier_terme + deuxieme_terme + troisieme_terme
+    # alpha = alpha_init
+    # for j in range(10**5):
+    #     alpha_candidat = alpha_init * b**j
+    #     if f(beta + alpha_candidat * delta_beta) <= f(beta) + alpha_candidat * sigma * D:
+    #         if alpha_candidat > alpha:
+    #             alpha = alpha_candidat
+    return alpha_init
