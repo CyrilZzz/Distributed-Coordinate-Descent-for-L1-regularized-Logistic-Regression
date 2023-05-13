c86eb6a4a702b5ba2cd9731f4d1eaae42bd73d18
mmathieu <mathis.mathieu@ensae.fr>
2023-05-13 10:58:15 +0000

    final update

--- performance.py
+++ performance.py
@@ -3,11 +3,12 @@ import pandas as pd
 import time
 
 data = pd.read_csv('dataset.csv')
+model = DistributedLassoLogReg(1)
+model.preprocessing(data, 'TenYearCHD')
 
 # We try different numbers of partitions and measure the training time
 for i in range(1, 17, 3):
     model = DistributedLassoLogReg(i)
-    model.preprocessing(data, 'TenYearCHD')
     start_time = time.time()
     model.fit('train_data.csv')
     end_time = time.time()
